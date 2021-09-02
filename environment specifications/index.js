f14651a55d876dfc5fa2baf5b23ade9d8ea83f20
CheeseCheesson <kira7999@gmail.com>
2021-09-02 07:22:15 +0300

    added exercise search elements

--- environment specifications/index.js
+++ environment specifications/index.js
@@ -1,6 +1,8 @@
+let main = document.querySelector(".main");
+
 let windowHeigth = window.innerHeight;
 let windowWidth = window.innerWidth;
-let navigator = window.navigator.userAgent
+let navigator = window.navigator.userAgent;
 let platform = window.navigator.platform;
 let link = window.location.href;
 
@@ -13,3 +15,26 @@ arr2.push(platform);
 arr2.push(link);
 
 console.log(arr2);
+let childNodes = [];
+for (let i = 0; i < document.body.childNodes.length; i++) {
+  childNodes.push(document.body.childNodes[i]);
+}
+
+for (let key of childNodes) {
+  let p = document.createElement("p");
+  p.textContent = key;
+  main.append(p);
+}
+
+console.log(childNodes);
+let trueArray = Array.from(childNodes);
+
+console.log(trueArray);
+
+
+let table = document.body.children[0].children[1].children[1].firstElementChild
+
+for(let i = 0; i < table.rows.length; i++){
+    table.rows[i].cells[i].style.background ='blue'
+    table.rows[i].cells[i].style.color ='white'
+}
